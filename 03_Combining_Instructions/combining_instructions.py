825eeb31ee98bd982ab841df24942d8ecec835c0
Yankho <ysaliji023@student.wethinkcode.co.za>
2024-10-07 04:52:19 +0200

    created combining instrusctions exercise

---
+++ 03_Combining_Instructions/combining_instructions.py
@@ -0,0 +1,44 @@
+def max_three():
+    """
+    Write a Python function to find the Max of three numbers.
+    Use the following as test input in STDIN
+
+    Input:
+    ```
+    1
+    8
+    6
+    ```
+    """
+
+    def max_of_two( x, y ):
+        if x > y:
+            return x
+        return y
+    def max_of_three( x, y, z ):
+        #enter your code here
+        return ""
+
+    num1 = int(input("Please enter num1"))
+    #enter your code here
+    print("The max of the three numbers is:")
+
+
+def between_3_and_9(n):
+    """
+    Write a Python function to check whether a number falls between 3 and 9
+    """
+
+    if n in range():
+        #enter code here
+        print(n)
+    else:
+        print("The number is outside the given range.")
+
+
+if __name__ == "__main__":
+    """
+    Run the entire program from here
+    """
+    # max_three()
+    # between_3_and_9(n)
